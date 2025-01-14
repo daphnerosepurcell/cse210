c186e1ab8439960bced93e3bfe95a38203f79bd9
daphnerosepurcell <purcelldaphnerose@gmail.com>
2025-01-13 18:41:02 -0700

    made this

---
+++ sandbox/Sandbox/name.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("What is your first name? ");
+         tring first = Console.Readline;
+        Console.Write("What is your last name? ");
+        string last = Condole.Readline;
+
+        Console.ReadLine($"Your name is {last}, {first} {last}.")
+    }
+}
