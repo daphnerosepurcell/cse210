311063b90a6e357f26bb2be9c9c3705c75d28c36
daphnerosepurcell <purcelldaphnerose@gmail.com>
2025-01-16 16:21:24 -0700

    Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 101);
+
+        
+        int numberguess = -1;
+        
+
+
+        while (magic != numberguess)
+
+        {
+            Console.Write("What is your guess? ");
+            numberguess = int.Parse(Console.ReadLine());
+
+             if (magic > numberguess)
+            {
+                Console.WriteLine("higher");
+            }
+            else if (numberguess > magic)
+            {
+                Console.WriteLine("Lower");
+            }
+            else 
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+    }   
+    
 }
