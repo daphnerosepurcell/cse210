23f0e588c946f5e228134dc6d8c4abe979b6af8f
daphnerosepurcell <purcelldaphnerose@gmail.com>
2025-01-16 19:43:23 -0700

    republishing to correct location, prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,35 @@
 using System;
 
-class Program
+namespace game
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep2 World!");
+        static void Main(string[] args)
+        {
+            Console.Write("What is your grade?: ");
+            
+            int grade = int.Parse(Console.ReadLine());
+
+            if (grade >= 90)
+            {
+                Console.WriteLine("You made an A.");
+            }
+            else if (grade >= 80 && < 90 )
+            {
+                Console.WriteLine("You made an B.");
+            }
+             else if (grade >= 70 && < 80 )
+            {
+                Console.WriteLine("You made an C.");
+            }
+             else if (grade >= 60 && <70 )
+            {
+                Console.WriteLine("You made an D.");
+            }
+            else grade < 60
+            {
+                Console.WriteLine("You made an F.");
+            }
+        }
     }
 }
