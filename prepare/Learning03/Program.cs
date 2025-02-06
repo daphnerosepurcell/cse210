bd48993fb4d72a82a87bc9338c9c6182d664368b
daphnerosepurcell <purcelldaphnerose@gmail.com>
2025-02-05 18:11:32 -0700

    changes numebring

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,8 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Fraction f1 = new Fraction();
-        Fraction f2 = new Fraction(5);
+        Fraction f1 = new Fraction(1, 1);
+        Fraction f2 = new Fraction(5, 1);
         Fraction f3 = new Fraction(3, 4);
         Fraction f4 = new Fraction(1, 3);
 
